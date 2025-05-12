aae493636353c361565eb1b3fac4ff62cdae910a
Adilson krischanski <adilson.krischanski@outlook.com>
2025-05-11 21:52:41 -0300

    generate with param state

--- src/main/java/com/codedev/api/generators/controllers/CEPBRAZILIANGeneratorController.java
+++ src/main/java/com/codedev/api/generators/controllers/CEPBRAZILIANGeneratorController.java
@@ -2,6 +2,7 @@ package com.codedev.api.generators.controllers;
 
 import com.codedev.api.generators.services.CEPBRAZILIANGeneratorService;
 import com.codedev.api.generators.services.CPFCNPJGeneratorService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +19,14 @@ public class CEPBRAZILIANGeneratorController {
         return ResponseEntity.ok(generatedCEP);
     }
 
-    @GetMapping("/generate-randon-cep-estado")
-    public ResponseEntity<String> generateRandomCEPState(@RequestParam(required = true) String estado) {
-        String generatedCEP = CEPBRAZILIANGeneratorService.generatePostalCode(estado);
+    @GetMapping("/generate-random-cep-estado")
+    public ResponseEntity<String> generateRandomCEPState(@PathParam("estado") String estado) {
+        String generatedCEP = CEPBRAZILIANGeneratorService.generatePostalCode(estado.toUpperCase());
+
+        if (generatedCEP == null) {
+            return ResponseEntity.badRequest().body("Estado inválido ou não suportado.");
+        }
+
         return ResponseEntity.ok(generatedCEP);
     }
 
