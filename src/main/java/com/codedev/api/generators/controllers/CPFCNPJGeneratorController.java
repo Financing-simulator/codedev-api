fcdfb0d966222e6335ca28ec7f2785ad0cffdd6f
Gabriel Godinho <gabriel234417@gmail.com>
2025-05-28 20:26:49 -0300

    [feature] Adição dos validadores de CPF e CNPJ

--- src/main/java/com/codedev/api/generators/controllers/CPFCNPJGeneratorController.java
+++ src/main/java/com/codedev/api/generators/controllers/CPFCNPJGeneratorController.java
@@ -2,25 +2,32 @@ package com.codedev.api.generators.controllers;
 
 import com.codedev.api.generators.services.CPFCNPJGeneratorService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
+@RequestMapping("/cpf-cnpj")
 public class CPFCNPJGeneratorController {
 
-    @GetMapping("/generate-random-cpf")
+    @GetMapping("/generate-cpf")
     public ResponseEntity<String> generateRandomCPF() {
         String generatedCPF = CPFCNPJGeneratorService.generateRandomCPF();
 
         return ResponseEntity.ok(generatedCPF);
     }
 
-    @GetMapping("/generate-random-cnpj")
+    @GetMapping("/generate-cnpj")
     public ResponseEntity<String> generateRandomCNPJ() {
         String generatedCNPJ = CPFCNPJGeneratorService.generateRandomCNPJ();
 
         return ResponseEntity.ok(generatedCNPJ);
     }
+
+    @GetMapping("/validate")
+    public ResponseEntity<Boolean> validateCPFOrCNPJ(@RequestParam(name = "cpf") String value) {
+        boolean isValid = value.length() < 14 ? CPFCNPJGeneratorService.isValidCPF(value) : CPFCNPJGeneratorService.isValidCNPJ(value);
+
+        return ResponseEntity.ok(isValid);
+    }
+
 }
--- src/main/java/com/codedev/api/generators/services/CPFCNPJGeneratorService.java
+++ src/main/java/com/codedev/api/generators/services/CPFCNPJGeneratorService.java
@@ -45,6 +45,45 @@ public class CPFCNPJGeneratorService {
         );
     }
 
+    public static boolean isValidCPF(String cpf) {
+        if (cpf == null || !cpf.matches("\\d{11}") && !cpf.matches("\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}"))
+            return false;
+
+        cpf = cpf.replaceAll("\\D", "");
+
+        if (cpf.chars().distinct().count() == 1) return false;
+
+        int[] digits = new int[11];
+        for (int i = 0; i < 11; i++) {
+            digits[i] = Character.getNumericValue(cpf.charAt(i));
+        }
+
+        int dv1 = calculateVerifierDigit(digits, 10);
+        int dv2 = calculateVerifierDigit(digits, 11);
+
+        return digits[9] == dv1 && digits[10] == dv2;
+    }
+
+    public static boolean isValidCNPJ(String cnpj) {
+        if (cnpj == null || !cnpj.matches("\\d{14}") && !cnpj.matches("\\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2}"))
+            return false;
+
+        cnpj = cnpj.replaceAll("\\D", "");
+
+        if (cnpj.chars().distinct().count() == 1) return false;
+
+        int[] digits = new int[14];
+
+        for (int i = 0; i < 14; i++) {
+            digits[i] = Character.getNumericValue(cnpj.charAt(i));
+        }
+
+        int dv1 = calculateVerifierDigit(digits, new int[]{5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2});
+        int dv2 = calculateVerifierDigit(digits, new int[]{6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2});
+
+        return digits[12] == dv1 && digits[13] == dv2;
+    }
+
     private static int calculateVerifierDigit(int[] cpf, int initialWeight) {
         int sum = 0;
 
